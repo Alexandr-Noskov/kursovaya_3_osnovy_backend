f87613c1c5c19679f913bfeff75c91f44d2aab54
Alexandr Noskov <122940182+Alexandr-Noskov@users.noreply.github.com>
2023-05-28 21:00:31 +0300

    Na4alo

---
+++ main.py
@@ -0,0 +1,17 @@
+filename = 'operations.json'
+
+
+def main():
+    FILTERED_EMPTY_FROM = True
+    COUNT_LAST_VALUES = 5
+
+    data = get_data(filename)
+    data = get_filtered_data(data, filter_empty_from=FILTERED_EMPTY_FROM)
+    data = get_last_values(data, count_last_values=COUNT_LAST_VALUES)
+    data = get_formatted_data(data)
+    print("INFO: Вывод транзакций...")
+    for row in data:
+        print(row, end='\n\n')
+
+if __name__ == "__main__":
+       main()
