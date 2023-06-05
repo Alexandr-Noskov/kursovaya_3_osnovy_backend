1294bf04fd6941076dc23b7bc9e72347944af132
Alexandr Noskov <san9nosko8@gmail.com>
2023-06-05 23:53:19 +0300

    @fixture

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,3 +1,5 @@
+import pytest
+
 from utils import get_data, get_filtered_data, get_formatted_data, get_last_values
 
 
@@ -8,15 +10,17 @@ def test_get_data():
 # ispravleniee
 
 def test_get_filtered_data(item):
-    assert get_filtered_data(item)
+    assert len(get_filtered_data(item))
 
-def test_get_values():
-    data = get_data('../operations.json')
+
+def test_get_values(item):
+    data = get_data(item)
     assert get_last_values(data, 2)[0]['date'] == "2023-03-23T01:09:46.296404"
     assert len(get_last_values(data, 5)) == 5
 
-def test_get_formatted_data():
-    data = get_data('operations.json')
+
+def test_get_formatted_data(item):
+    data = get_data(item)
     print(get_formatted_data(data[0]))
     assert get_formatted_data(data[0]) == """2019-08-26 10:50:58.294041 Перевод организации
 Maestro 1596 83** **** 5199 -> Счет **9589
